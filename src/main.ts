d6a6c8f51617797e1e5e5b3abdafb38636742a79
wpzhicay <wpzhicay@sudamericano.edu.ec>
2025-06-07 20:43:59 -0500

    backend

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const app = await NestFactory.create(AppModule, { cors: true }); // 👈 habilita CORS aquí
+  await app.listen(3000);
 }
 bootstrap();
+
